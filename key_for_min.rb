11aa8ad5eae81f9b0ea591a3a7adb72ec1a25a61
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-02 18:00:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
+
 def key_for_min_value(name_hash)
   smallest_value = 0 
   smallest_key = nil
   name_hash.collect do |key, value| 
     if smallest_value == 0 || value < smallest_value 
       smallest_value = value
+      binding
       smallest_key = key
     end 
   end
